f2a211662b612d30e2d676e28a8129008b124774
Philip Newborough <corenominal@corenominal.org>
2016-01-29 18:27:54 +0000

    Add the bare plugin file

---
+++ corenominal-wp-custom-post-types.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: corenominal's Custom Post Types
+ * Description: This plugin provides custom post types and taxonomies for my site.
+ * Plugin URI: https://github.com/corenominal/corenominal-wp-custom-post-types
+ * Author: Philip Newborough
+ * Author URI: http://corenominal.org
+ * Version: 0.0.1
+ * License: GPLv2
+ */
