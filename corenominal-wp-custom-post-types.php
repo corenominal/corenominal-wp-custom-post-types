90af2c0842160ee7997d559aad41a623768fc203
Philip Newborough <corenominal@corenominal.org>
2016-01-31 10:15:14 +0000

    Widgets FTW

--- corenominal-wp-custom-post-types.php
+++ corenominal-wp-custom-post-types.php
@@ -10,21 +10,35 @@
  */
 
 /**
- * Custom post type and taxonomy for links
+ * Custom post type, taxonomy & widget for links
  */
 require_once( plugin_dir_path( __FILE__ ) . 'post-types/link.php' );
 require_once( plugin_dir_path( __FILE__ ) . 'taxonomies/link-tag.php' );
+require_once( plugin_dir_path( __FILE__ ) . 'widgets/recent-links.php' );
 
 /**
- * Custom post type and taxonomies for snippets
+ * Custom post type, taxonomies & widget for snippets
  */
 require_once( plugin_dir_path( __FILE__ ) . 'post-types/snippet.php' );
 require_once( plugin_dir_path( __FILE__ ) . 'taxonomies/snippet-tag.php' );
 require_once( plugin_dir_path( __FILE__ ) . 'taxonomies/snippet-language.php' );
+require_once( plugin_dir_path( __FILE__ ) . 'widgets/recent-snippets.php' );
 
 /**
- * Custom post type and taxonomies for doodle
+ * Custom post type, taxonomies and widget for doodles
  */
 require_once( plugin_dir_path( __FILE__ ) . 'post-types/doodle.php' );
 require_once( plugin_dir_path( __FILE__ ) . 'taxonomies/doodle-tag.php' );
 require_once( plugin_dir_path( __FILE__ ) . 'taxonomies/doodle-medium.php' );
+require_once( plugin_dir_path( __FILE__ ) . 'widgets/latest-doodle.php' );
+
+/**
+ * A recent posts widget
+ */
+require_once( plugin_dir_path( __FILE__ ) . 'widgets/recent-posts.php' );
+
+/**
+ * A recent widget to display RSS feeds
+ */
+require_once( plugin_dir_path( __FILE__ ) . 'widgets/subscriptions.php' );
+
---
+++ widgets/subscriptions.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ * Create widget to produce links to RSS feeds
+ */
+class corenominal_rss_widget extends WP_Widget{
+	
+	function __construct()
+	{
+		parent::__construct(
+			'corenominal_rss_widget', // Base ID
+			'corenominal - RSS Links', // Name
+			array('description' => __( 'Displays links to your RSS feeds.' ))
+		   );
+	}
+
+	function update( $new_instance, $old_instance )
+	{
+			$instance = $old_instance;
+			$instance['title'] = strip_tags($new_instance['title']);
+			return $instance;
+	}
+
+	function form( $instance )
+	{
+		if( $instance )
+		{
+			$title = esc_attr( $instance['title'] );
+		}
+		else
+		{
+			$title = '';
+		}
+		?>
+			<p>
+			<label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title', 'corenominal_rss_widget'); ?></label>
+			<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" />
+			</p>
+		<?php
+	}
+
+	function widget( $args, $instance )
+	{
+		extract( $args );
+		$title = apply_filters('widget_title', $instance['title']);
+		
+		if ( $title )
+		{
+			$title =  $before_title . $title . $after_title;
+		}
+		
+		$count_posts = wp_count_posts( 'post' );
+		$published_posts = $count_posts->publish;
+		$count_posts = wp_count_posts( 'link' );
+		$published_links = $count_posts->publish;
+		$count_posts = wp_count_posts( 'snippet' );
+		$published_snippets = $count_posts->publish;
+		$count_posts = wp_count_posts( 'doodle' );
+		$published_doodles = $count_posts->publish;
+		$feeds = '';
+		
+		$feeds .= '<li><i class="fa fa-rss"></i> <a href="' . site_url() . '/feed/">Subscribe to All</a></li>';
+
+		if( $published_posts )
+		{
+			$feeds .= '<li><i class="fa fa-rss"></i> <a href="' . site_url() . '/feed/?post_type=post">Subscribe to Posts</a></li>';
+		}
+		
+		if( $published_links )
+		{
+			$feeds .= '<li><i class="fa fa-rss"></i> <a href="' . site_url() . '/feed/?post_type=link">Subscribe to Links</a></li>';
+		}
+
+		if( $published_snippets )
+		{
+			$feeds .= '<li><i class="fa fa-rss"></i> <a href="' . site_url() . '/feed/?post_type=snippet">Subscribe to Snippets</a></li>';
+		}
+
+		if( $published_doodles )
+		{
+			$feeds .= '<li><i class="fa fa-rss"></i> <a href="' . site_url() . '/feed/?post_type=doodle">Subscribe to Doodles</a></li>';
+		}
+		
+		if($feeds != '')
+		{
+			echo $before_widget;
+			echo $title;
+			echo '<ul class="subscriptions">';
+			echo $feeds;
+			echo '</ul>';
+			echo $after_widget;
+		}
+	}
+
+}
+
+function register_corenominal_rss_widget()
+{
+    register_widget('corenominal_rss_widget');
+}
+add_action('widgets_init', 'register_corenominal_rss_widget');
